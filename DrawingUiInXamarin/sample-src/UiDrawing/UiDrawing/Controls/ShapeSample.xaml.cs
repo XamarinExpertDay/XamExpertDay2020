a550dc445bac8e4e4941c91eee88a03e962fac9d
Konrad Mueller <k.l.mueller@outlook.com>
2020-10-08 23:04:27 +0200

    Add files for ui drawing in xamarin

---
+++ DrawingUiInXamarin/sample-src/UiDrawing/UiDrawing/Controls/ShapeSample.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UiDrawing.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShapeSample : ContentView
+    {
+        public ShapeSample()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DrawingUiInXamarin/sample-src/UiDrawing/UiDrawing/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Reflection;
+using SkiaSharp;
+
+namespace UiDrawing
+{
+    public static class Utility
+    {
+        public static SKBitmap LoadSomeGuy()
+        {
+            var resourceID = "UiDrawing.Resources.some-guy.png";
+            var assembly = typeof(Utility).Assembly;
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceID))
+            {
+                return SKBitmap.Decode(stream);
+            }
+        }
+    }
+}
